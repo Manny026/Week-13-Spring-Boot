feec7fe60b41649416b8150f0c1e53ca495a8dcd
Manuel Peralta <wowmanny26@gmail.com>
2023-05-01 14:46:56 -0700

    first commit

--- src/main/java/com/promineotech/jeeps/entity/Jeeps.java
+++ src/main/java/com/promineotech/jeeps/entity/Jeeps.java
@@ -1,6 +1,9 @@
 package com.promineotech.jeeps.entity;
 
 import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,17 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Jeeps {
-
   private Long modelPK;
-  
   private JeepModel modelId;
-  
   private String trimLevel;
-  
   private int numDoors;
-  
   private int wheelSize;
-  
   private BigDecimal basePrice;
   
+  @JsonIgnore
+  public Long getModelPK() {
+	  return modelPK;
+  }
 }
